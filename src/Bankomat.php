dcfa815a49c6afe568b779b1b07eb7ec9147a548
Matsavx <super.matvey0111@gmail.com>
2021-06-21 03:59:53 +1000

    version 0.1.0

---
+++ src/Bankomat.php
@@ -0,0 +1,13 @@
+<?php
+ class Bankomat {
+     private $money_value;
+     public function __construct($money_value) {
+         $this->money_value = $money_value;
+     }
+     public function get_money($money_value){
+         $this->money_value += $money_value;
+     }
+     public function get_fucking_money() {
+         return $this->money_value;
+     }
+ }
---
+++ src/Chelovek.php
@@ -0,0 +1,17 @@
+<?php
+
+class Chelovek {
+    private $name;
+    private $money;
+
+    public function __construct($name, $money)
+    {
+        $this->name = $name;
+        $this->money = $money;
+    }
+
+    public function add_money($Bankomat, $money_value) {
+        $this->money = $this->money-$money_value;
+        $Bankomat->get_money($money_value);
+    }
+}
